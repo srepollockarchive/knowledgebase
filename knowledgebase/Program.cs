bb2afbfe966ef27ac1ce4b6c4d5078c4c88898b2
Spencer Pollock <srepollock@gmail.com>
2017-10-26 09:27:16 -0700

    Added C# Project

---
+++ knowledgebase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace knowledgebase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
